60e1652dbdc70e953fdf0fe0898749b7aac426e3
Bergi-e <jayden.bergstrome@gmail.com>
2026-01-18 21:19:45 -0800

    Cleaned up script.js to be more modular and reworked how a given note is deleted.

--- COMP4537/labs/1/js/script.js
+++ COMP4537/labs/1/js/script.js
@@ -7,58 +7,61 @@ if (addBtn) {
     addBtn.addEventListener('click', () => {
         const newNote = new Note('');
         notes.push(newNote);
-        localStorage.setItem('notes', JSON.stringify(notes));
+        updateStorage();
         notesWrapper.appendChild(newNote.createDOMElement());
     });
 }
 
 
 class Note {
-    constructor(content, id) {
+    constructor(content, id, isReadOnly) {
         this.content = content;
         this.id = id || Date.now(); // Creates a unique ID based on timestamp
+        this.isReadOnly = isReadOnly;
     }
 
     // Method to create the HTML for this note (textarea + remove button)
     createDOMElement() {
         this.container = document.createElement('div');
-
         const elem = document.createElement('textarea');
+
+        elem.readOnly = this.isReadOnly
         elem.value = this.content;
         elem.addEventListener('input', () => {
             this.content = elem.value;
-            localStorage.setItem('notes', JSON.stringify(notes));
+            updateStorage();
         });
         this.container.appendChild(elem);
 
-        // addBtn is essentially a read/write page clause
-        if (addBtn) {
-
+        if (!this.isReadOnly) {
             // Display the remove button ONLY in writer.html
             const removeBtn = document.createElement('button');
             removeBtn.textContent = messages.removeButton;
             removeBtn.addEventListener('click', () => this.remove());
             this.container.appendChild(removeBtn);
-        } else {
-            elem.readOnly = true;
         }
         return this.container;
     }
     
-    // Method to remove itself
+    // Method to remove itself from the list
     remove() {
         // Remove from DOM
         this.container.remove();
-        // Trigger a save to update localStorage
-        const notes = JSON.parse(localStorage.getItem('notes'));
-        const filteredNotes = notes.filter(note => note.id !== this.id);
-        localStorage.setItem('notes', JSON.stringify(filteredNotes));
-        
+
+        const index = notes.findIndex(note => note.id === this.id);
+
+        if (index !== -1) {
+            notes.splice(index, 1);
+            updateStorage();
+        }
     }
 }
 
 notes.forEach(noteData => {
-    const note = new Note(noteData.content, noteData.id);
+    const note = new Note(noteData.content, noteData.id, !addBtn);
     notesWrapper.appendChild(note.createDOMElement());
-
 });
+
+function updateStorage() {
+    localStorage.setItem('notes', JSON.stringify(notes));
+}
