c0b0915cddbd733acafd5603b59d87339b2f3428
Bergi-e <jayden.bergstrome@gmail.com>
2026-02-01 13:19:05 -0800

    Implemented simple classes for both en and Utils to gather the greeting along with the current date (as a string)

---
+++ COMP4537/labs/3/lang/en/en.js
@@ -0,0 +1,9 @@
+class Lang {
+    constructor() {
+        this.greeting = 
+        "Hello %1, What a beautiful day." 
+        + " Server current date and time is ";
+    }
+}
+
+module.exports = new Lang();
